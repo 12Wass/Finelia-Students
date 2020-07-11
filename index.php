21c4b88096cbb9564f169fad1be9bc99511c004b
Wassim <wassimdah@gmail.com>
2020-07-11 16:36:43 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <?php include_once('navbar.php'); ?>
+    <h1 class="text-center">Index</h1>
+
+</body>
+
+</html>
